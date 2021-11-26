1cd560d5a6bf4c303efbde138a2761e49ac46d57
Fabien Bounoir <71253960+FabienBounoir@users.noreply.github.com>
2021-11-27 00:16:45 +0100

    📲 supression fond de test

--- src/App.svelte
+++ src/App.svelte
@@ -228,7 +228,7 @@
 	left: 0;
 	width: 100%;
 	height: 100%;
-    background-image: url("./assets/background.png");
+    /* background-image: url("./assets/background.png"); */
     background-size: cover;
 }
 
